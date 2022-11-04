641c2cdec2d30694db6302dc28a01ec24e826782
koral <koral762@gmail.com>
2022-11-04 17:37:06 +0200

    first commit

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>HI KORAL</Text>
+      <Text>HI KORAL</Text>
+      <Text>HI KORAL</Text>
       <StatusBar style="auto" />
     </View>
   );
